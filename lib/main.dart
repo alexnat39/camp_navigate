d563c5b56854eea59fb577b566a9c40042c2189d
Alex Natalenko <natalenko2001@gmail.com>
2021-06-26 07:46:18 +0300

    added core folders

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,98 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'Pages/home_page.dart';
 
-void main() {
+void setupLocator() {
+  // GetIt.instance.registerLazySingleton(() => SlidersService());
+  // GetIt.instance.registerLazySingleton(() => UserService());
+  // GetIt.instance.registerLazySingleton(() => ServiceService());
+  // GetIt.instance.registerLazySingleton(() => ClinicService());
+  // GetIt.instance.registerLazySingleton(() => InsuranceService());
+  // GetIt.instance.registerLazySingleton(() => GalleryService());
+  // GetIt.instance.registerLazySingleton(() => ReviewService());
+  // GetIt.instance.registerLazySingleton(() => AppointmentService());
+  // GetIt.instance.registerLazySingleton(() => DocumentService());
+  // GetIt.instance.registerLazySingleton(() => FileService());
+  // GetIt.instance.registerLazySingleton(() => CertificatesService());
+  // GetIt.instance.registerLazySingleton(() => PushNotificationsService());
+  // GetIt.instance.registerLazySingleton(() => PartnerService());
+  // GetIt.instance.registerLazySingleton(() => FirebaseService());
+
+
+}
+
+
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  setupLocator();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+class MyApp extends StatefulWidget {
+  MyApp();
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyAppState createState() => _MyAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyAppState extends State<MyApp> {
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+
+  @override
+  void initState() {
+    super.initState();
   }
+
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    return WillPopScope(
+      onWillPop: () async {
+        if (Navigator.of(context).userGestureInProgress)
+          return false;
+        else
+          return true;
+      },
+      child: MultiProvider(
+        providers: [],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          builder: (context, child) {
+            return ScrollConfiguration(
+              behavior: NoGlowBehavior(),
+              child: child!,
+            );
+          },
+
+          theme: ThemeData(
+            fontFamily: 'GothamPro',
+          ),
+          initialRoute: '/home',
+          routes: {
+            "/home": (context) => HomePage(),
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+/**
+ * this function is used above to disable glow when scrolling
+ */
+class NoGlowBehavior extends ScrollBehavior {
+  @override
+  Widget buildViewportChrome(
+      BuildContext context, Widget child, AxisDirection axisDirection) {
+    return child;
   }
 }
