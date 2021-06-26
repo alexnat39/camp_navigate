e303025fe1c5e64f84980a363058e75f9e30e231
alisajoangarcia <abgarcia@purdue.edu>
2021-06-26 08:16:29 -0500

    added mental health activity service class

--- lib/Pages/TrackerPages/mental_health_tracker_page.dart
+++ lib/Pages/TrackerPages/mental_health_tracker_page.dart
@@ -1,18 +1,24 @@
+import 'package:camp_navigate/CustomWidgets/TrackerPageWidgets/chosen_option_widget.dart';
 import 'package:camp_navigate/CustomWidgets/UniversalWidgets/AppBarWidgets/container_app_bar.dart';
-import 'package:camp_navigate/CustomWidgets/custom_widgets_homepage.dart';
+import 'package:camp_navigate/CustomWidgets/UniversalWidgets/custom_universal_button.dart';
+import 'package:camp_navigate/CustomWidgets/UniversalWidgets/show_alert.dart';
+import 'package:camp_navigate/CustomWidgets/UniversalWidgets/show_loading_dialog.dart';
+import 'package:camp_navigate/CustomWidgets/UniversalWidgets/show_snack_bar.dart';
 import 'package:camp_navigate/Helpers/custom_colors.dart';
+import 'package:camp_navigate/Services/physical_activity_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:line_icons/line_icons.dart';
+import 'package:get_it/get_it.dart';
 
 class MentalHealthTrackerPage extends StatefulWidget {
-  // const MentalHealthTrackerPage({Key? key}) : super(key: key);
 
   @override
   _MentalHealthTrackerPageState createState() => _MentalHealthTrackerPageState();
 }
 
 class _MentalHealthTrackerPageState extends State<MentalHealthTrackerPage> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
---
+++ lib/Services/mental_health_activity_service.dart
@@ -0,0 +1,8 @@
+class MentalHealthActivityService {
+  // returns 0 if success, 1 if error occurred
+  Future<int> sendPhysicalActivitiesList(String userId, List<String> activities) async {
+
+    return 0;
+  }
+
+}
