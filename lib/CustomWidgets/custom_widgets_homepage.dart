7e7e5c554239e55d53bf270b9ad0498c630fb042
alisajoangarcia <abgarcia@purdue.edu>
2021-06-26 01:00:06 -0500

    added 3 icon buttons

---
+++ lib/CustomWidgets/custom_widgets_homepage.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
+
+class CustomPhysicalButton extends StatefulWidget {
+
+  final VoidCallback func;
+  CustomPhysicalButton({required this.func});
+
+  @override
+  _CustomPhysicalButtonState createState() => _CustomPhysicalButtonState();
+}
+
+class _CustomPhysicalButtonState extends State<CustomPhysicalButton> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 90,
+        height: 90,
+        child: RawMaterialButton(
+          fillColor: Colors.red[300],
+          shape: CircleBorder(),
+          elevation: 0,
+          child: Icon(
+            Icons.fitness_center,
+            size: 45.0,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.pushNamed(context, '/physical');
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class CustomNutritionButton extends StatefulWidget {
+
+  final VoidCallback func;
+  CustomNutritionButton({required this.func});
+
+  @override
+  _CustomNutritionButtonState createState() => _CustomNutritionButtonState();
+}
+
+class _CustomNutritionButtonState extends State<CustomNutritionButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 90,
+        height: 90,
+        child: RawMaterialButton(
+          fillColor: Colors.yellow[300],
+          shape: CircleBorder(),
+          elevation: 0,
+          child: Icon(
+            LineIcons.carrot,
+            size: 45.0,
+            color: Colors.white,
+          ),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
+
+class CustomMentalHealthButton extends StatefulWidget {
+
+  final VoidCallback func;
+  CustomMentalHealthButton({required this.func});
+
+  @override
+  _CustomMentalHealthButtonState createState() => _CustomMentalHealthButtonState();
+}
+
+class _CustomMentalHealthButtonState extends State<CustomMentalHealthButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 90,
+        height: 90,
+        child: RawMaterialButton(
+          fillColor: Colors.green[300],
+          shape: CircleBorder(),
+          elevation: 0,
+          child: Icon(
+            LineIcons.brain,
+            size: 45.0,
+            color: Colors.white,
+          ),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
